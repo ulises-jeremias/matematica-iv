1ce986c4623a02b69a70ed3ff5030e4c1033191d
ulises-jeremias <ulisescf.24@gmail.com>
2018-11-22 16:53:50 -0300

    Updated speech example

--- examples/speech_recognition/sketch.js
+++ examples/speech_recognition/sketch.js
@@ -1,17 +1,16 @@
 function setup() {
-    noCanvas();
+  noCanvas();
 
-    let lang = navigator.language || 'en-US';
-    let speechRec = new p5.SpeechRec(lang, gotSpeech);
-  
-    let continuous = true;
-    let interim = false;
-    speechRec.start(continuous, interim);
-  
-    function gotSpeech() {
-      if (speechRec.resultValue) {
-        createP(speechRec.resultString);
-      }
+  let lang = navigator.language || 'en-US';
+  let speechRec = new p5.SpeechRec(lang, gotSpeech);
+
+  let continuous = true;
+  let interim = true;
+  speechRec.start(continuous, interim);
+
+  function gotSpeech() {
+    if (speechRec.resultValue) {
+      createP(speechRec.resultString);
     }
-  
   }
+}
